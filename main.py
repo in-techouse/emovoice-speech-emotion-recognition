8b94e6b5986cd17702302087c393d003954ef996
Tayyaba <tayyabasaeed160@gmail.com>
2021-03-21 00:48:54 -0700

    Windows Packages exported

--- main.py
+++ main.py
@@ -10,11 +10,5 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text('received')
+        await websocket.send_text(data)
 
-# pip install uvicorn uvloop websockets pyrebase onesignal-sdk
-# pip install absl-py astunparse cached-property cachetools certifi chardet flatbuffers gast google google-auth-oauthlib google-pasta
-# pip install grpcio h5py idna importlib-metadata joblib Keras Keras-Preprocessing Markdown numpy oauthlib opt-einsum pandas protobuf
-# pip install pyasn1 pyasn1-modules python-dateutil pytz PyYAML requests requests-oauthlib rsa scikit-learn scipy six sklearn speechpy 
-# pip install tensorboard tensorboard-plugin-wit tensorflow-estimator termcolor threadpoolctl typing-extensions urllib3 Werkzeug wrapt zipp
-# python setup.py install
