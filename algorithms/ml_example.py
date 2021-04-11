7da36ebd5eb6a2484ef0b17c04cbf9a3da16dd83
In-TecHouse <team.dev.man@gmail.com>
2021-04-11 14:03:58 +0500

    get voice message & detect emotion done, working on send response

--- algorithms/ml_example.py
+++ algorithms/ml_example.py
@@ -4,20 +4,17 @@ This example demonstrates how to use `NN` model ( any ML model in general) from
 """
 import urllib.request
 from pydub import AudioSegment
-from common import extract_data
+from algorithms.common import extract_data
 from speechemotionrecognition.mlmodel import NN
 from speechemotionrecognition.utilities import get_feature_vector_from_mfcc
 
 
-def ml_example():
-    fileUrl = 'https://firebasestorage.googleapis.com/v0/b/emovoice.appspot.com/o/ChatAudios%2F169166106166136108205128219127%2F1616835089442.m4a?alt=media&token=5ff44ead-fd91-4186-ad53-8c18df92edb6'
-    name = '1613979505836'
-    filename = name + '.m4a'
-    urllib.request.urlretrieve(fileUrl, filename)
+def ml_example(url, sourceName):
+    print("URL is: ", url)
+    print("Source Name is: ", sourceName)
+    filename = sourceName
+    urllib.request.urlretrieve(url, filename)
     print('file downloaded')
-    wma_version = AudioSegment.from_file(filename, 'm4a')
-    wma_version.export(name + '.wav', format="wav")
-    print('file converted')
 
     to_flatten = True
     x_train, x_test, y_train, y_test, _ = extract_data(flatten=to_flatten)
@@ -26,6 +23,7 @@ def ml_example():
     model.train(x_train, y_train)
     model.evaluate(x_test, y_test)
 
+    # filename = testingFileName + '.wav'
     # 0 => Neutral
     # Neutral File 1
     # filename = './dataset/Neutral/03a02Nc.wav'
@@ -34,7 +32,7 @@ def ml_example():
 
     # 1 => Angry
     # Angry File 1
-    filename = './dataset/Angry/03a04Wc.wav'
+    # filename = './dataset/Angry/03a04Wc.wav'
     # Angry File 2
     # filename = './dataset/Angry/10a04Wa.wav'
 
@@ -50,10 +48,11 @@ def ml_example():
     # Sad File 2
     # filename = './dataset/Sad/16a05Tb.wav'
 
-    print('prediction', model.predict_one(
-        get_feature_vector_from_mfcc(filename, flatten=to_flatten)),
-        'Actual 1')
+    # print('prediction', model.predict_one(
+    #     get_feature_vector_from_mfcc(filename, flatten=to_flatten)),
+    #     'Actual 1')
+    return model.predict_one(get_feature_vector_from_mfcc(filename, flatten=to_flatten))
 
 
-if __name__ == "__main__":
-    ml_example()
+# if __name__ == "__main__":
+#     ml_example()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import HTMLResponse
 from starlette.websockets import WebSocket
+from algorithms.ml_example import ml_example
 
 app = FastAPI()
 
@@ -10,5 +11,21 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(data)
+        print("Data is", data)
+        strArray = data.split("*")
+        print("Str Array is: ", strArray)
+        url = strArray[0]
+        sourceName = strArray[1]
+        messageId = strArray[2]
+        emotion = ml_example(url, sourceName)
+        print("Emotion is: ", emotion)
+        print("Emotion type is: ", type(emotion))
+        print("Emotion is: ", len(emotion))
+        print("Emotion is: ", emotion.size)
+        print("Emotion is: ", emotion.shape)
+        # 0 => Neutral
+        # 1 => Angry
+        # 2 => Happy
+        # 3 => Sad
+        await websocket.send_text(messageId)
 
